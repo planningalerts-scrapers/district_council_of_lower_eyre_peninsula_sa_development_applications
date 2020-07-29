f2d9ae90f0c9811cf7fbc5df01b929b68c106d22
MichaelBone <Michael.Bone@senet.com.au>
2020-07-29 17:23:16 +0930

    Reversed the order of the PDFs.

--- scraper.ts
+++ scraper.ts
@@ -593,7 +593,7 @@ async function main() {
         let pdfUrl = new urlparser.URL(element.attribs.href, DevelopmentApplicationsUrl).href;
         if (pdfUrl.toLowerCase().includes("register") && pdfUrl.toLowerCase().includes(".pdf"))
             if (!pdfUrls.some(url => url === pdfUrl))  // avoid duplicates
-                pdfUrls.push(pdfUrl);
+                pdfUrls.unshift(pdfUrl);
     }
 
     // Always parse the most recent PDF file and randomly select one other PDF file to parse.
